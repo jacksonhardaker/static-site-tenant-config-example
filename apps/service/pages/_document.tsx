45a0a32c85958c795b65ea0aa0ddc86a2ced97a8
Jackson Hardaker <jackson@bitdrift.io>
2024-01-16 11:05:49 -0500

    fuckup.

--- apps/service/pages/_document.tsx
+++ apps/service/pages/_document.tsx
@@ -3,10 +3,12 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <body>
+      <Head>
+        <link rel="preload" as="script" href="/config.js" />
         {/* eslint-disable-next-line @next/next/no-sync-scripts */}
         <script src="/config.js" type="text/javascript"></script>
+      </Head>
+      <body>
         <Main />
         <NextScript />
       </body>
